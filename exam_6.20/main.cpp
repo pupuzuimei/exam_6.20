c983cf1728055e5ffa1b1d87a29813bc93539e20
pupuzuimei <50659089+pupuzuimei@users.noreply.github.com>
2019-06-26 13:02:47 +0800

    exam_6.26

--- exam_6.20/main.cpp
+++ exam_6.20/main.cpp
@@ -10,7 +10,15 @@
 #include<fstream>
 #include<string>
 using namespace std;
-
+	score play(10);
+    double read[10];
+    double a[10];
+    double pp[10];
+    double qq[10];
+    double t;
+	int i,j,number;
+	char Full Name,sex,school;
+	double achievement[10];
 int main() {
     char name[20];
     int num=0;
@@ -21,25 +29,52 @@ int main() {
     ifstream enf("/Users/s20181106278/Desktop/enf.txt");
     if(onf.is_open())
     {
-        cout<<"file OK"<<endl;
-        onf>>num>>name>>sex>>sch;
-        cout<<num<<"      "<<name<<endl;
+        for(i=0;i<7;i++)
+		{
+			for(j=0;j<5;j++)
+			{
+				onf>>number>>" ">>Full Name>>" ">>sex>>" ">>school;
+			}
+		} 
         onf.close();
     }
     if(enf.is_open())
     {
-        int num1=0;
-        enf>>num1;
-        char name1[20];
-        for(int i=0;i<7;i++)
+        int line;
+		for(line=2;line<6;line++)
+		{
+			in.seekg(0,ios::beg);
+			for(int i=0;i<line<i++)
+			{
+				in.getline(store,sizeof(store));
+				std::istringstream iss(store);
+			}
+			i=0;
+			while (iss >> read[i])
+            {
+                achievement[i] = read[i];
+                i++;
+            }
+            pp[line]=play.sum(a); 
+            qq[line]=play.aver(a); 
+		}
+        for (int m = 0; m<5; m++)
         {
-            enf>>name1[i];
+            for (int n = 0; n<5-m-1; n++)
+           {
+               if (pp[n]>pp[n+ 1])
+              {
+                  t = pp[n];
+                  pp[n] = pp[n + 1];
+                  pp[n + 1] = t;
+              } 
+           }
         }
+       enf.close();
     }
     if(inf.is_open())
     {
-        inf <<"c="<<num<<endl;
-        cout<< "c="<<num<<endl;
+       
         inf.close();
         
     }
